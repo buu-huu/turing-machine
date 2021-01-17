6d33a96321623f909c4e8d94b966e822c76aa9d6
buu-huu <luca-schwarz@outlook.de>
2021-01-17 18:07:34 +0100

    Class for program instructions and extended functionality from read_program() for reading in

---
+++ src/instruction.py
@@ -0,0 +1,7 @@
+class Instruction:
+    def __init__(self, condition_state, condition_read, result_state, result_write, result_move):
+        self.condition_state = condition_state
+        self.condition_read = condition_read
+        self.result_state = result_state
+        self.result_write = result_write
+        self.result_move = result_move
--- src/turing.py
+++ src/turing.py
@@ -1,4 +1,5 @@
 from tape import *
+from instruction import *
 
 
 def read_program(path):
@@ -6,14 +7,27 @@ def read_program(path):
         instructions = f.readlines()
     instructions = [x.strip() for x in instructions]
 
-    return instructions
+    instruction_list = []
+    for instruction in instructions:
+        conditions = instruction.split('>')[0].split(',')
+        result = instruction.split('>')[1].split(',')
+
+        new_instruction = Instruction(conditions[0],
+                                      conditions[1],
+                                      result[0],
+                                      result[1],
+                                      result[2])
+
+        instruction_list.append(new_instruction)
+
+    return instruction_list
 
 
 def main():
-    program = read_program('turing_programs/double_ones.txt')
-
     tape = Tape(10, 0)
     tape.set_initial_values([1, 1, 1])
+
+    instructions = read_program('turing_programs/double_ones.txt')
 
 
 if __name__ == '__main__':
